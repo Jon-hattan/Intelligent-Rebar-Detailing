b3de078b6652f60c992feafb6fc0194e4307c9ef
Jon-hattan <jonchew123mel@gmail.com>
2025-07-26 10:36:38 +0800

    Use relative path and forward slash for pdf_path

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ import Preprocessors.Rectangle_subtraction as RS
 import Box_grouper2 as  BG
 import draw_arrows as DA
 
-pdf_path = r"C:\Users\jonch\Downloads\131101-WIP12-DR-S-5123 & 5124_commented_20250414 1 (1).pdf"
+pdf_path = "./unprocessed_pdfs/131101-WIP12-DR-S-5123 & 5124_commented_20250414.pdf"
 
 # Load rectangles and void boxes
 rectangles = Grey_box_detector.find_bounding_boxes(pdf_path)
